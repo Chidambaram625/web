5857de833b635d546bf1dd1afb97c2590a3e1252
Chidambaram625 <126405329+Chidambaram625@users.noreply.github.com>
2024-11-14 20:40:33 +0530

    Update bg_remove.py

--- api/bg_remove.py
+++ api/bg_remove.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template, send_from_directory
-import redis
+# import redis
 
 # from telegram import Update
 # from telegram.ext import Application, ContextTypes, CommandHandler
